4560613ff948e90e0e760a1678239730dac561b9
unknown <okashatanoli12345@gmail.com>
2020-06-27 16:20:48 +0500

    calculator project done

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+function calculation(number){
+   var resultScreen = document.getElementById("input")
+   input.value += number
+}
+
+
+function result(){
+   var input = document.getElementById("input")
+   input.value = eval(input.value)
+}
+
+function clearInput(){
+   var input = document.getElementById("input")
+   input.value = ""
+}
+function lastDelete(){
+   var input = document.getElementById("input")
+   input.value = input.value.substring(0,input.value.length-1);
+
+}
+function remove(){
+   var input = document.getElementById("input")
+   input.value 
+}
+
